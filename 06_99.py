47c594cc769712c73b72f2d38d845c35965c9a9d
rhunter4 <robin.hunter@jacobs.com>
2021-03-19 11:26:12 -0700

    done 7

--- 06_99.py
+++ 06_99.py
@@ -1,7 +1,6 @@
 # ninty nine problems
 # source; https://wiki.python.org/moin/ProblemSets/99%20Prolog%20Problems%20Solutions
 
-
 # starting with 6
 # Flaten a nested list structure
 
---
+++ template.py
@@ -0,0 +1,14 @@
+# ninty nine problems
+# source; https://wiki.python.org/moin/ProblemSets/99%20Prolog%20Problems%20Solutions
+
+
+def main():
+
+    return
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as ex:
+        print(ex)
