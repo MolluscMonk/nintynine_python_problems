b1a2edebc5ea226f6986834b96ddfd27fc9f7450
rhunter4 <robin.hunter@jacobs.com>
2021-03-19 11:46:09 -0700

    done 9

--- 09_99.py
+++ 09_99.py
@@ -1,8 +1,8 @@
 # ninty nine problems
 # source; https://wiki.python.org/moin/ProblemSets/99%20Prolog%20Problems%20Solutions
 
-# Problem 8
-# Eliminate consecutive duplicates of list elements
+# 9
+# Problem 9: Pack consecutive duplicates of list elements into sublists
 
 
 l1 = [1, 1, 1, 2, 3]
@@ -14,8 +14,8 @@ def main():
 
     list_list = [l1, l2, l3]
     for lst in list_list:
-        fresh_list = remove_dupe(lst)
-        print(fresh_list)
+        fresh_list, dupe_list = remove_dupe(lst)
+        print(fresh_list, '\n', dupe_list)
 
     return
 
@@ -23,13 +23,15 @@ def main():
 def remove_dupe(lst: list):
     rng = len(lst) - 1
     offset = 0
+    dupes = []
     for i in range(rng):
         j = i + 1 - offset
         k = i - offset
         if lst[k] == lst[j]:
-            lst.pop(k)
+            dupe = lst.pop(k)
+            dupes.append(dupe)
             offset += 1
-    return lst
+    return lst, dupes
 
 
 if __name__ == "__main__":
